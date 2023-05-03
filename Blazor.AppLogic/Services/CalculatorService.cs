c4028ee349b599341ccc94f9c1af2e829ada8526
Dtc-Xtreme <Dtc-Xtreme@CI9-10900K>
2023-05-03 15:06:06 +0200

    mvc added

---
+++ Blazor.AppLogic/Services/CalculatorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.AppLogic.Services
+{
+    public class CalculatorService : ICalculatorService
+    {
+        public string Calculate()
+        {
+            return "test";
+        }
+    }
+}
---
+++ Blazor.AppLogic/Services/ICalculatorService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.AppLogic.Services
+{
+    public interface ICalculatorService
+    {
+
+        public string Calculate();
+    }
+}
