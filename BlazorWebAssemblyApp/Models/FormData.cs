c0e5ae189df2014383b8050e8e3b1d5a882c4f48
Dtc-Xtreme <Dtc-Xtreme@CI9-10900K>
2023-05-03 13:23:07 +0200

    Add project files.

---
+++ BlazorWebAssemblyApp/Models/FormData.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAssemblyApp.Models
+{
+    public class FormData
+    {
+
+        [Required]
+        public string ID { get; set; }
+
+        [Range(1, 9)]
+        public int Quantity { get; set; }
+    }
+}
