3e3262b7211c43d0855b97401a2ba391e6f21b80
Dtc-Xtreme <Dtc-Xtreme@CI9-10900K>
2023-05-18 15:33:28 +0200

    js interops

---
+++ BlazorServerApp/wwwroot/js/javascript.js
@@ -0,0 +1,10 @@
+﻿window.Alert = function (message) {
+    alert(message);
+}
+
+window.returnStringAsync = () => {
+    DotNet.invokeMethodAsync('BlazorServerApp', 'ReturnString')
+        .then(data => {
+            alert(data);
+        });
+};
