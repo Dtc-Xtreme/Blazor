b19718e6843b41f4838f46c6db2f12247e6e14aa
Steven Kazmierczak <steven.kazmierczak@benvitec.be>
2023-06-05 10:59:50 +0200

    forms added

---
+++ BlazorServerApp/Models/FormDataModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerApp.Models
+{
+    public class FormDataModel
+    {
+        [Required]
+        public string Text { get; set; }
+
+        [Range(5, int.MaxValue)]
+        public int Number { get; set; }
+    }
+}
