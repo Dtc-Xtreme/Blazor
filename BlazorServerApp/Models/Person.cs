6a73405a099096bee302ce49485b2748b869310c
Dtc-Xtreme <Dtc-Xtreme@CI9-10900K>
2023-06-10 14:25:38 +0200

    test

--- BlazorServerApp/Models/Person.cs
+++ BlazorServerApp/Models/Person.cs
@@ -8,10 +8,10 @@ namespace BlazorServerApp.Models
         [Required]
         public int Id { get; set; }
 
-        [Required]
-        [MinLength(2)]
-        [MaxLength(20)]
         [FirstLetterCapital(ErrorMessage = "This value should start with a captial letter.")]
+        [Required]
+        [MinLength(1)]
+        [MaxLength(20)]
         public string Name { get; set; }
 
         [Range(80,200)]
